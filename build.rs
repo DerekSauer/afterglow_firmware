31ec95edb357a44655da8e7ab23253f0388da5e6
Derek Sauer <dereksauer.ca@gmail.com>
2025-07-10 13:06:39 -0400

    fix (build): Missing license specifier in build script.
    Signed-off-by: Derek Sauer <dereksauer.ca@gmail.com>

--- build.rs
+++ build.rs
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2025 Derek Sauer
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
+
 fn main() {
     println!("cargo::rustc-link-arg=-Tlinkall.x");
     println!("cargo::rustc-link-arg=-Tdefmt.x");
